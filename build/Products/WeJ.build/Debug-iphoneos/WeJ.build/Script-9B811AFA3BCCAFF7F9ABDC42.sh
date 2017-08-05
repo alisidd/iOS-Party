dba3fabbe00074610fad5b51cd6f8a87eee4f68d
Ali Siddiqui <alinsiddiqui@gmail.com>
2017-08-05 23:02:08 +0300

    Fix folder organization

---
+++ build/Products/WeJ.build/Debug-iphoneos/WeJ.build/Script-9B811AFA3BCCAFF7F9ABDC42.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+"${SRCROOT}/Pods/Target Support Files/Pods-WeJ/Pods-WeJ-resources.sh"
+
---
+++ build/Products/WeJ.build/Debug-iphoneos/WeJ.build/Script-D1DBB02DE533B6567D4964A5.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+"${SRCROOT}/Pods/Target Support Files/Pods-WeJ/Pods-WeJ-frameworks.sh"
+
