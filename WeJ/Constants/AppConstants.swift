85f87fcb44f787f8be434d7d64a70cc8b1918749
Ali Siddiqui <alinsiddiqui@gmail.com>
2018-06-02 18:28:50 -0700

    Add missing files

---
+++ WeJ/Constants/AppConstants.swift
@@ -0,0 +1,25 @@
+//
+//  AppConstants.swift
+//  WeJ
+//
+//  Created by Ali Siddiqui on 8/9/17.
+//  Copyright © 2017 Mohammad Ali Siddiqui. All rights reserved.
+//
+
+import Foundation
+
+struct AppConstants {
+    
+    static let orange = UIColor(red: 1, green: 166/255, blue: 35/255, alpha: 1)
+    static let black = UIColor(red: 26/255, green: 26/255, blue: 26/255, alpha: 1)
+    static let darkerBlack = UIColor(red: 20/255, green: 20/255, blue: 20/255, alpha: 1)
+    
+    static let red = UIColor(red: 255/255, green: 71/255, blue: 62/255, alpha: 1)
+    static let green = UIColor(red: 117/255, green: 203/255, blue: 39/255, alpha: 1)
+    
+    static let doneButtonWidth = 51
+    static let doneButtonHeight = 40
+    
+    static let minimumLaunchesBeforeReview = 5
+    
+}
